3032432c56eafc1a3a07d9cc0d6b54a5fc1cb3ca
restiyanada <restiyana.dwi@gmail.com>
2018-05-22 12:31:17 +0700

    arduino to MySQL file

---
+++ arduinotomysql.py
@@ -0,0 +1,24 @@
+import serial
+import MySQLdb
+#Establishing connection 
+db = MySQLdb.connect("localhost","root","","arduino") 
+#format ("LocalHost","arduino","Database-Password","Database-Name")
+
+cursor = db.cursor() #Opening the cursor to Database
+
+port = 'COM4'         #Declare the serial port
+print "Connecting...", port            #This confirms the declaration
+arduino = serial.Serial(port, 9600)  #Will read the serial data
+print "arduino detected"               #Confirms the device
+ 
+data = arduino.readline()  #Read data from the device
+
+#Inserting the data into table
+cursor.execute("INSERT INTO motionsensor (Voltage) VALUES (Voltage)", (data)) 
+#format ("INSERT INTO <table> (colomn) VALUES(type)",(initialized value)) 
+#Here initialized value is data, which is used to read device.
+
+db.commit()     #Commit to insert the data
+	
+cursor.close()  #Close the cursor
+	 
